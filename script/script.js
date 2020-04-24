3a8f10535eaf7067ee6b0f109f5ae2f3d9950be0
Uliana Bobrova <uliana.bobrova@gmail.com>
2020-04-24 18:09:34 +0300

    Change opacity value and create tabs

--- script/script.js
+++ script/script.js
@@ -77,7 +77,6 @@ const toggleMenu = () => {
             
 };
 
-
 toggleMenu();
 
 
@@ -86,10 +85,10 @@ const togglePopUp = () => {
     //получает popup окна
     const popup = document.querySelector('.popup'),
     //получаем popup кнопки
-    popupBtn = document.querySelectorAll('.popup-btn'),
+        popupBtn = document.querySelectorAll('.popup-btn'),
     //получаем крестик на popup окне
-    popUpClose = document.querySelector('.popup-close'),
-    popupContent = document.querySelector('.popup .popup-content');
+    // popUpClose = document.querySelector('.popup-close'),
+        popupContent = document.querySelector('.popup .popup-content');
   
    
 //Анимация кнопок, свойство opacity
@@ -104,7 +103,7 @@ const togglePopUp = () => {
                     if (count >= 1) {
                         clearInterval(opInterval);  
                     }
-            }, 80);
+            }, 200);
         } else {
             popup.style.display = 'block';
         }
@@ -112,20 +111,73 @@ const togglePopUp = () => {
 
 //Нажатие на кнопки
     popupBtn.forEach((elem) => 
-    // elem.addEventListener('click', () => {
-    //     popup.style.display = 'block';
         elem.addEventListener('click', popupOpacity));
     
 //Закрытие модального окна    
-    popUpClose.addEventListener('click', () => {
-        popup.style.display = 'none';
+    popup.addEventListener('click', (event) => {
+        let target = event.target;
+
+        if(target.classList.contains('popup-close')){
+            popup.style.display = 'none';
+        } else {
+            target = target.closest('.popup-content');
+
+            if(!target) {
+                popup.style.display = 'none';
+            }
+        }
+          
     });
 };
 
 togglePopUp();
 
+//Табы
 
+const tabs = () => {
+    const tabHeader = document.querySelector('.service-header'),
+    tab = tabHeader.querySelectorAll('.service-header-tab'),
+    tabContent = document.querySelectorAll('.service-tab');
 
+//функция меняющая контент, перебирает все табы,показывает выбранный, передаем в нее индекс нашего таба
+const toggleTabContent = (index) => {
+    for(let i = 0; i < tabContent.length; i++) {
+//полученный индекс сравниваем с индексом tab
+        if(index === i) {
+            //таб с таким индексом будет показываться на странице
+            tab[i].classList.add('active');
+            tabContent[i].classList.remove('d-none');
+        } else {
+            tab[i].classList.remove('active');
+            tabContent[i].classList.add('d-none');
+        }
+    }
+};
+
+//второй вариант этой функции 20 урок 18 минута. С методом closest()
+    tabHeader.addEventListener('click', (event) => {
+        let target = event.target;
+
+            while(target !== tabHeader) {
+                //проверяем, что кликнули именно по tab
+                if (target.classList.contains('service-header-tab')) {
+            //проверяем на какой tab мы кликнули
+                tab.forEach((item, i) => {
+                //если элемент соответствует нашему target
+                    if(item === target) {
+                    toggleTabContent(i);
+                    }
+                });
+                return;
+            }
+//в while проверили,что target не является tabHeader,зашли в цикл,если target не с классом service-header-tab,даем target класс родителя
+                target = target.parentNode;
+            }
+    });
+
+};
+
+tabs();
 
 
 
