0e1d408d9c33d87226e1cabdeb8e2de4dd85c7b8
Uliana Bobrova <uliana.bobrova@gmail.com>
2020-04-28 08:51:11 +0300

    Calculator done

--- script/script.js
+++ script/script.js
@@ -317,35 +317,94 @@ slider();
 
 //Блок Наша команда
 
-const command = () => {
+    const command = () => {
     const commandPeople = document.querySelector('#command');
     let photo = document.querySelectorAll('.command__photo');
     
     photo.forEach((elem) => elem.addEventListener('mouseenter', (event) => {
-    event.target.dataset.currentImg = event.target.src;
-    event.target.src = event.target.dataset.img;
-    })
-  );
+        event.target.dataset.currentImg = event.target.src;
+        event.target.src = event.target.dataset.img;
+        })
+    );
 
     photo.forEach((elem) => elem.addEventListener('mouseleave', (event) => {
-    event.target.src = event.target.dataset.currentImg ;
-    })
-  );
+        event.target.src = event.target.dataset.currentImg ;
+        })
+    );
 
-};
-command();
+    };
+
+    command();
 
 //Калькулятор
 
-const counter = () => {
-    const square = document.querySelectorAll('.calc-block > input');
+// const counter = () => {
+//     const square = document.querySelectorAll('.calc-block > input');
 
-    square.forEach((elem) => elem.addEventListener('input', (event) => {
-        event.target.value = event.target.value.replace(/\D/gi, '');
-    })
-    );
-};
+//     square.forEach((elem) => elem.addEventListener('input', (event) => {
+//         event.target.value = event.target.value.replace(/\D/gi, '');
+//     })
+//     );
+// };
 
-counter();
+// counter();
+
+    const calc = (price = 100) => {
+        const calcBlock = document.querySelector('.calc-block'),
+        calcType = document.querySelector('.calc-type'),
+        calcSquare = document.querySelector('.calc-square'),
+        calcDay = document.querySelector('.calc-day'),
+        calcCount = document.querySelector('.calc-count'),
+        totalValue = document.getElementById('total');
+
+    const countSum = () => {
+    let total = 0,
+    countValue = 1,
+    dayValue = 1;
+    //находим выбранный нами селект,выводим нужную нам options через соответствующий индекс
+    const typeValue = calcType.options[calcType.selectedIndex].value,
+         squareValue = +calcSquare.value;
+
+//если количество помещений > 1, то получаем десятую долю         
+        if(calcCount.value > 1) {
+        countValue += (calcCount.value - 1) / 10;
+        }
+
+//срок исполнения. CalсDay вообще существует и <5 или <10
+        if(calcDay.value && calcDay.value < 5) {
+        dayValue *= 2;
+        }else if(calcDay.value && calcDay.value < 10) {
+        dayValue *= 1.5;
+        }    
+//пока пользователь не заполнит 2 первых поля, в total должен выводиться 0
+        if(typeValue && squareValue) {
+        total = price * typeValue * squareValue * countValue * dayValue;
+        } 
+
+    //вывод на страницу
+        totalValue.textContent = total;
+    };
+        
+    calcBlock.addEventListener('change', (event) => {
+            const target = event.target;
+//1-ый способ: ищем класс, на котором произошло событие, перечислив все 4 эл-та
+            // if (target.matches('.calc-type') || target.matches('.calc-square') ||
+            // target.matches('.calc-day') || target.matches('.calc-count')) {
+            // }
+//2-ой способ: сравниваем таргет с полученными элементами
+            // if(target === calcType || target === calcSquare || target === calcDay ||
+            //      target === calcCount) {
+            //          console.log(1);
+            //      }
+//3-ий способ - проверяем на тег select и инпута
+        if(target.matches('select') || target.matches('input')) {
+                //функция,которая считает итоговую цену
+            countSum();
+            }
+
+    });
+    };
+//при вызове калькулятора передаем ему цену price
+    calc(100);
 
 });
